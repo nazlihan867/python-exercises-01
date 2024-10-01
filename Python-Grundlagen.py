e7aa59649048c7ce7cf2c1c71551ca919c0e874d
NaTa <nazlihan-atan@web.de>
2024-10-01 23:50:57 +0200

    Python-Grundlagen.py

---
+++ Python-Grundlagen.py
@@ -0,0 +1,11 @@
+# Definiere die Variablen
+first_name = "Nazlihan"
+last_name = "Atan"
+birth_year = 1990  # Beispieljahr
+has_license = True  # oder False, je nach deinem Führerscheinstatus
+
+# Gib die Variablenwerte auf der Konsole aus
+print("Vorname:", first_name)
+print("Nachname:", last_name)
+print("Geburtsjahr:", birth_year)
+print("Hat Führerschein:", has_license)
